dd20f16aabc2e59d72597111e0a527a05cf15906
zevamaru <sebastiancamara@gmail.com>
2025-11-27 23:33:20 -0300

    definiendo estructura del proyecto

---
+++ src/pwa.js
@@ -0,0 +1,11 @@
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    console.log("New content available — reload?");
+  },
+  onOfflineReady() {
+    console.log("App ready offline ✨");
+  },
+});
